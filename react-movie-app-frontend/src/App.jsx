e0997a710f04c1354c9868c676530af3deccd7f5
Czech Louie <czech.sadiangabay@amcsgroup.com>
2025-03-10 20:30:27 +0800

    added context

--- react-movie-app-frontend/src/App.jsx
+++ react-movie-app-frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import "./App.css";
+import "./css/App.css";
+import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
 
 function App() {
-  const movieNumber = 2;
-
   return (
-    <>
-      <Home />
-    </>
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
